f3f559ac539b992affea6f38a56f3354a54533b0
Samuel Olokor <samuelolokor228@gmail.com>
2025-10-31 22:35:29 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-2649276101-7e09f",
+  "appId": "1:673028982195:web:cb3b36ab156db56e9377c6",
+  "apiKey": "AIzaSyACZAhsJ0SV9n2r63kCQmKPWF3cZ6apTCk",
+  "authDomain": "studio-2649276101-7e09f.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "673028982195"
+};
